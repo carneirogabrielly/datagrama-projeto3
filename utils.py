71d77934b6aceb0b0570ae32dbcab957b53e8ad9
EnzoSilva14 <enzodrbs@al.insper.ed.br>
2024-08-30 17:56:31 -0300

    utils

--- utils.py
+++ utils.py
@@ -1,3 +1,8 @@
+from aplicacao import *
+from enlace import enlace
+
+getData = enlace.getData()
+
 def fragmenta(mensagem : bytes):
     tamanho = len(mensagem)
     inteiro = tamanho // 50
@@ -11,7 +16,50 @@ def fragmenta(mensagem : bytes):
 
     return fragmentos
 
+def carrega_pacote(com1):
+    head, tamanho_head = com1.getData(12)
+    tamanho_payload = int.from_bytes(head[2], byteorder='big')
+
+        #pegando o payload
+    payload,_ = com1.getData(tamanho_payload)
+
+        #pegando o eop
+    eop,_ = com1.getData(3)
+
+    return head, payload, eop
+
+#----------------------CLIENT------------------------
 def make_pack(fragmentos : list):
+    head = bytearray(12)
+    eop = b'x46\x49\x4D'
+    head[0] = len(fragmentos)
+    pacotes = []
+    for fragmento in fragmentos:
+        index = fragmento.index()
+        head[1] = index + 1
+        head[2] = len(fragmento)
+        head[3] = 0
+        payload = fragmento
+        pacote = bytearray([head, payload, eop])
+        pacotes.append(pacote)
     
+    return pacotes
+#---------------------SERVER-------------------------
+
+
+
+def verifica_pack(head, payload, eop):
+
+    qtd_pacotes = head[0]
+    numero_pacote = head[1]
+    tamanho_payload = head[2]
+
+    
+
+    if head[]
+        
+
+
+     
 
 
