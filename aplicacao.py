d20e5e8012859974333a8e38b0bc93f2a4975bb9
EnzoSilva14 <enzodrbs@al.insper.ed.br>
2024-09-05 17:37:24 -0300

    finalizado comunicacao

--- aplicacao.py
+++ aplicacao.py
@@ -13,6 +13,7 @@
 from enlace import *
 import time
 import numpy as np
+from utils import *
 
 # voce deverá descomentar e configurar a porta com através da qual ira fazer comunicaçao
 #   para saber a sua porta, execute no terminal :
@@ -22,9 +23,9 @@ import numpy as np
 #use uma das 3 opcoes para atribuir à variável a porta usada
 #serialName = "/dev/ttyACM0"           # Ubuntu (variacao de)
 #serialName = "/dev/tty.usbmodem1411" # Mac    (variacao de)
-serialName = "COM11"                  # Windows(variacao de)
+serialName = "COM15"                  # Windows(variacao de)
 
-
+imageW = 'imgs/img.png'
 def main():
     try:
         print("Iniciou o main")
@@ -36,49 +37,54 @@ def main():
         # Ativa comunicacao. Inicia os threads e a comunicação seiral 
         com1.enable()
         #Se chegamos até aqui, a comunicação foi aberta com sucesso. Faça um print para informar.
+
         print("Abriu a comunicação")
-        
+        print("esperando 1 byte de sacrifício")
+        rxBuffer, nRx = com1.getData(1)
+        com1.rx.clearBuffer()
+        time.sleep(.1)
            
-                  
-        #aqui você deverá gerar os dados a serem transmitidos. 
-        #seus dados a serem transmitidos são um array bytes a serem transmitidos. Gere esta lista com o 
-        #nome de txBuffer. Esla sempre irá armazenar os dados a serem enviados.
-        
-        #txBuffer = imagem em bytes!
-        txBuffer = b'\x12\x13\xAA'  #isso é um array de bytes
-       
-        print("meu array de bytes tem tamanho {}" .format(len(txBuffer)))
-        #faça aqui uma conferência do tamanho do seu txBuffer, ou seja, quantos bytes serão enviados.
-       
-            
-        #finalmente vamos transmitir os todos. Para isso usamos a funçao sendData que é um método da camada enlace.
-        #faça um print para avisar que a transmissão vai começar.
-        #tente entender como o método send funciona!
-        #Cuidado! Apenas trasmita arrays de bytes!
-               
-        
-        com1.sendData(np.asarray(txBuffer))  #as array apenas como boa pratica para casos de ter uma outra forma de dados
-          
-        # A camada enlace possui uma camada inferior, TX possui um método para conhecermos o status da transmissão
-        # O método não deve estar fincionando quando usado como abaixo. deve estar retornando zero. Tente entender como esse método funciona e faça-o funcionar.
-        txSize = com1.tx.getStatus()
-        print('enviou = {}' .format(txSize))
-        
-        #Agora vamos iniciar a recepção dos dados. Se algo chegou ao RX, deve estar automaticamente guardado
-        #Observe o que faz a rotina dentro do thread RX
-        #print um aviso de que a recepção vai começar.
-        
-        #Será que todos os bytes enviados estão realmente guardadas? Será que conseguimos verificar?
-        #Veja o que faz a funcao do enlaceRX  getBufferLen
-      
-        #acesso aos bytes recebidos
-        txLen = len(txBuffer)
-        rxBuffer, nRx = com1.getData(txLen)
-        print("recebeu {} bytes" .format(len(rxBuffer)))
-        
-        for i in range(len(rxBuffer)):
-            print("recebeu {}" .format(rxBuffer[i]))
-        
+       #-------- VERIFICANDO SE ESTÁ ATIVO ------------
+        com1.sendData(b'01') #confirmando que estou vivo
+
+        #---------RECEBENDO AS MENSAGENS -------------
+        lista_payload = []
+        head, payload, eop = carrega_pacote(com1)
+        print('epa')
+        tamanho_da_mensagem = head[0]
+
+        verifica = verifica_pack(head,eop, 1)
+        print(verifica)
+        if verifica:
+                lista_payload.append(payload)
+                pacote = make_pack_server(verifica)
+                com1.sendData(pacote)
+        else:
+            pacote = make_pack_server(verifica)
+            com1.sendData(pacote)
+        print('passou')
+        for i in range(tamanho_da_mensagem - 1):
+            head, payload, eop = carrega_pacote(com1)
+            verifica = verifica_pack(head, eop, i+2)
+            if verifica:
+                lista_payload.append(payload)
+                pacote = make_pack_server(verifica)
+                com1.sendData(pacote)
+            else:
+                pacote = make_pack_server(verifica)
+                com1.sendData(pacote)
+                
+
+        payload_completo = b''.join(lista_payload)
+        f = open(imageW, 'wb')
+        f.write(payload_completo)
+        #fecha arquivo de imagem
+        f.close()
+
+
+
+
+
 
             
     
--- utils.py
+++ utils.py
@@ -1,28 +1,32 @@
 from aplicacao import *
 from enlace import enlace
 
-getData = enlace.getData()
+#getData = enlace.getData()
 
-def fragmenta(mensagem : bytes):
+
+def fragmenta(mensagem: bytes):
     tamanho = len(mensagem)
-    inteiro = tamanho // 50
-    resto = tamanho % 50
+    inteiro = tamanho // 50  # Número de fragmentos completos
+    resto = tamanho % 50  # Quantidade de bytes restantes
     fragmentos = []
-    if inteiro > 0:    
-        for i in range(inteiro - 1):
-            fragmentos.append(mensagem[i:50 * (i + 1)])
-    if resto>0:
-        fragmentos.append(mensagem[50*inteiro:])
+
+    # Adicionar fragmentos de tamanho 50
+    for i in range(inteiro):
+        fragmentos.append(mensagem[i * 50: (i + 1) * 50])
+
+    # Adicionar fragmento com o restante
+    if resto > 0:
+        fragmentos.append(mensagem[inteiro * 50:])
 
     return fragmentos
 
+
 def carrega_pacote(com1):
     head, tamanho_head = com1.getData(12)
-    tamanho_payload = int.from_bytes(head[2], byteorder='big')
-
+    print(head[2])
+    tamanho_payload = head[2]
         #pegando o payload
     payload,_ = com1.getData(tamanho_payload)
-
         #pegando o eop
     eop,_ = com1.getData(3)
 
@@ -31,32 +35,50 @@ def carrega_pacote(com1):
 #----------------------CLIENT------------------------
 def make_pack(fragmentos : list):
     head = bytearray(12)
-    eop = b'x46\x49\x4D'
-    head[0] = len(fragmentos)
+    eop = b'\x46\x49\x4D'  # EOP correto
     pacotes = []
-    for fragmento in fragmentos:
-        index = fragmento.index()
-        head[1] = index + 1
-        head[2] = len(fragmento)
-        head[3] = 0
-        payload = fragmento
-        pacote = bytearray([head, payload, eop])
-        pacotes.append(pacote)
+
+    if len(fragmentos) > 255:
+        raise ValueError("Número de fragmentos não pode exceder 255.")
+
+    for i, fragmento in enumerate(fragmentos):
+        if len(fragmento) > 255:
+            raise ValueError(f"Fragmento {i + 1} excede o tamanho máximo de 255 bytes.")
+
+        head[0] = len(fragmentos)  # Número total de fragmentos (máximo 255)
+        head[1] = i + 1  # Índice do fragmento (começa do 1)
+        head[2] = len(fragmento)  # Tamanho do fragmento (máximo 255)
+        head[3] = 0  # Outro campo, ajuste conforme necessário
+
+        payload = fragmento  # O fragmento como payload
+        pacote = bytearray(head) + bytearray(payload) + eop  # Concatenar tudo
+        
+        pacotes.append(pacote)  # Adicionar o pacote à lista
     
     return pacotes
 #---------------------SERVER-------------------------
-
-
-
-def verifica_pack(head, payload, eop):
-
+def verifica_pack(head, eop, numero_esperado):
     qtd_pacotes = head[0]
     numero_pacote = head[1]
-    tamanho_payload = head[2]
-
+    if numero_esperado != numero_pacote:
+        print("ERRO -- Ordem errada de pacote")
+        return False
+    if eop != b'\x46\x49\x4D':
+        print("ERRO -- tamanho do payload diferente")
+        return False
+    return True
+        
+def make_pack_server(verifica):
+    head = bytearray(12)
+    eop = b'\x46\x49\x4D'
+    print(verifica)
+    if verifica:
+        head[3] = 0
+    else:
+        head[3] = 1
+    pacote = bytearray(head) + eop
+    return pacote
     
-
-    if head[]
         
 
 
